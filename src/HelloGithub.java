a160de67cb5070aba1b394bf150d754712e9a344
Yildiz <yldzgny34@gmail.com>
2023-03-14 14:12:14 +0100

    ikinci version

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -1,6 +1,6 @@
     public class HelloGithub {
     public static void main(String[] args) {
         System.out.println("Yildiz'dan merhaba");
-
+        System.out.println("ikinci yorumum");
     }
 }
